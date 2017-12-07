e6b3391fc3ffb94d1220cb3f0b2f4755566d4bce
Unknown <milosz9798@gmail.com>
2017-12-07 21:17:29 +0100

    Changes

---
+++ hello-bugs/Assets/Scripts/DeleteTrashScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngineInternal.Input;
+
+public class DeleteTrashScript : MonoBehaviour
+{
+    public string requiredTag;
+    public PointControllerScript pointController;
+    public void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.tag == requiredTag)
+            Destroy(other.gameObject);
+
+    }
+}
